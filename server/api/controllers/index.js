129d5cc3e45019ce06c814b1ef7b4af7d7ef90ac
Alexander Levchenko <alexander.levchenko@nixsolutions.com>
2017-12-02 18:07:52 +0200

    Created GET: /api/clocks

--- server/api/controllers/index.js
+++ server/api/controllers/index.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 
 exports.getTimezones = (req, res, next) => {
-  fs.readFile('api/timezones.json', 'utf8', (err, file) => {
+  fs.readFile('api/timezones.json', (err, file) => {
     if (err) {
       return next({ status: 404, message: 'No such file.' });
     }
 
     res.status(200).json({ timezones: JSON.parse(file) });
+  })
+};
+
+exports.getClocks = (req, res) => {
+  fs.readFile('api/clocks.json', (err, file) => {
+    if (err) {
+      return res.status(200).json({ clocks: [] });
+    }
+
+    res.status(200).json({ clocks: JSON.parse(file)});
   })
 };
--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -2,4 +2,5 @@ const controller = require('api/controllers');
 
 module.exports = (app) => {
   app.get('/api/timezones', controller.getTimezones);
+  app.get('/api/clocks', controller.getClocks);
 };
